65559c7ed169edfb01fef0c3ba45697c345e827c
partholon <9391253+partholon@users.noreply.github.com>
2025-04-19 03:52:44 +0800

    [BugFix] use beijing time instead of local time
    The UTC time obtained locally is determined by the system time and
    cannot obtain the real Beijing time. Therefore, it is modified to obtain
    the standard time from NTP. After that, we will correct the local time
    by adding a burden to the current time to ensure the accurate operation
    of the script.
    
    issue: #0

--- clickOnTime.py
+++ clickOnTime.py
@@ -4,82 +4,107 @@ import sys
 from pynput.mouse import Controller, Button
 from datetime import datetime
 import pytz
+import ntplib
+
 
 def setShoppingTime():
-    # TODO(parth): 后续时间应该由GUI录入
-    print('本次外观开售时间，请使用如下格式"2019-05-21 18:02:49"')
+    print('输入本次外观开售时间，请使用如下格式"2019-05-21 18:02:49"')
     t = input()
-    if t == '':
-        print('你必须指定一个北京时间')
+    if t == "":
+        print("你必须指定一个北京时间")
         sys.exit()
-    print('''
-预期将在北京时间 %s 模拟鼠标点击''' % t)
+    print(
+        """
+预期将在北京时间 %s 模拟鼠标点击"""
+        % t
+    )
     timeArray = time.strptime(t, "%Y-%m-%d %H:%M:%S")
     return time.mktime(timeArray)
 
+
 def setBuyButtonPosition(mouse):
-    print('''
+    print(
+        """
 /-------------------------------------------/
 /请将鼠标放置与购买按钮位置，3s后记录鼠标位置/
 /-------------------------------------------/
-''')
+"""
+    )
     time.sleep(3)
     x, y = mouse.position
     return x, y
 
-def checkTimeZone():
-    print('检查您系统的时间是否正确。')
-    # 获取中国时区
-    china_tz = pytz.timezone('Asia/Shanghai')
 
-    # 获取当前系统时间
-    local_time = datetime.now()
-    local_ftime = local_time.strftime('%Y-%m-%d %H:%M:%S')
+def tryGetTimeBurden():
+    print("检查您系统的时间是否正确……")
+    # Get Beijing time
+    china_tz = pytz.timezone("Asia/Shanghai")
+    ntp_client = ntplib.NTPClient()
+    for _ in range(5):  # 尝试5次
+        try:
+            # try get standard UTC time
+            ntp_response = ntp_client.request("pool.ntp.org")
+            utc_time = datetime.fromtimestamp(ntp_response.tx_time, tz=pytz.utc)
+            beijing_time = utc_time.astimezone(china_tz)
+            beijing_ftime = beijing_time.strftime("%Y-%m-%d %H:%M:%S")
+            # Get Local system time
+            local_time = datetime.now(china_tz)
+            local_ftime = local_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    # 获取当前北京时间
-    beijing_time = datetime.now(china_tz)
-    beijing_ftime = beijing_time.strftime('%Y-%m-%d %H:%M:%S')
-
-    # 打印当前系统时间和北京时间
-    print("当前系统时间：", local_ftime)
-    print("标准北京时间：", beijing_ftime)
-    # 比较两者是否相同
-    if local_ftime == beijing_ftime:
-        print("当前系统时间与北京时间相同，你可以正常使用该脚本。")
-        return True
+            # logger current time
+            print("当前系统时间：", local_ftime)
+            print("标准北京时间：", beijing_ftime)
+            # diff time
+            if local_ftime == beijing_ftime:
+                print("当前系统时间与北京时间相同，脚本将基于本地时间运行。")
+                return 0
+            else:
+                print("当前系统时间与北京时间不同，脚本将基于标准北京时间运行。")
+                return local_time.timestamp() - beijing_time.timestamp()
+        except ntplib.NTPException as ex:
+            print("校准时间失败，即将重试。" + str(ex))
+            time.sleep(1)
     else:
-        print("当前系统时间与北京时间不同，请调整您的系统设置。")
-        return False
+        print(
+            "无法获取标准北京时间，脚本将基于本地时间运行，请自行确定本地时间是否准确。"
+        )
+        return 0
+
 
 def main():
-    clickTimeStamp = setShoppingTime()
-    # print('clickTime=%s' % (clickTimeStamp))
+    # Use the UTC time to verify the local time and update the burden value
+    time_burden = tryGetTimeBurden()
+    # get target click time
+    clickTimeStamp = setShoppingTime() + time_burden
 
+    # record target click position
     mouse = Controller()
     x, y = setBuyButtonPosition(mouse)
-    print('购买按钮位置记录完毕，x=%s, y=%s' % (x, y))
+    print("购买按钮位置记录完毕，x=%s, y=%s" % (x, y))
 
-    # 休眠到开始点击前1s
+    # update sleep duration
+    print("准备休眠至开始购买前夕……")
     sleepTime = clickTimeStamp - time.time() - 1
     if sleepTime > 0:
-        print('休眠%s秒至购买开放前夕' % int())
+        print("休眠%s秒至购买开放1s" % int(sleepTime))
         time.sleep(sleepTime)
 
-    # 开始循环查询时间等待点击
-    print('开始等待购买开放')
+    # ready to click
+    print("开始等待购买开放")
     while time.time() < clickTimeStamp:
         pass
 
     # Just Buy It!
     mouse.position = (x, y)
     mouse.click(Button.left, 3)
-    nowTimeStamp = time.time()
+    nowTimeStamp = time.time() - time_burden
     localTime = time.localtime(nowTimeStamp)
     endTime = time.strftime("%Y-%m-%d %H:%M:%S", localTime)
-    print('%s 点击购买完毕' % endTime)
+    print("北京时间 %s 点击购买完毕" % endTime)
 
-if __name__ == '__main__':
-    if (not checkTimeZone()):
-        input("按任意键关闭程序...")
-    else:
-        main()
+    # waiting
+    input("按回车键退出...")
+
+
+if __name__ == "__main__":
+    main()
